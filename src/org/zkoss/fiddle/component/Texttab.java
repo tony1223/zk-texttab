00fd1acbf325aec468044775f5b6b2514552d76d
TonyQ Wang <tonylovejava@gmail.com>
2011-06-04 02:24:22 +0800

    fine tune appendChild rule and updating pom dependency

--- src/org/zkoss/fiddle/component/Texttab.java
+++ src/org/zkoss/fiddle/component/Texttab.java
@@ -7,13 +7,29 @@ import org.zkoss.zul.impl.api.InputElement;
 
 public class Texttab extends Tab {
 
+	public Texttab(){
+		super();
+	}
+	
+	public Texttab(String label){
+		super(label);
+	}
+	
+	public Texttab(String label, String image) {
+		super(label, image);
+	}
+	
 	protected boolean isChildable() {
 		return true;
 	}
 	
 	public void beforeChildAdded(Component child, Component refChild) {
 		if (!(child instanceof InputElement))
-			throw new UiException("Unsupported child for rows: "+child);
+			throw new UiException("Unsupported child for texttab: "+child);
+		
+		if(this.getChildren().size() >1)
+			throw new UiException("only allow one child for texttab: "+child);
+		
 	}
 	
 
